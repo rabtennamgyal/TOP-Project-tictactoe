ec22222d68efdf48fe1823be32f9de516c871e10
Rabten <rabten@protonmail.com>
2021-10-18 08:07:36 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function Player(name, symbol) {
+    return {name, symbol}
+}
+
+
+const GameBoard = (() => {
+    const board = []
+
+    const playerOne = Player('Player One')
+    const playerTwo = Player('Player Two')
+
+
+})()
